04d3aec13a04a2ffa6b6598748855c778c53b6d9
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-02 20:35:25 -0300

    feat: implement DefaultExchangeOperations for exchange rate currency and Bitcoin quotation

---
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/core/DefaultExchangeOperations.java
@@ -0,0 +1,39 @@
+package io.github.jvlealc.hgbrasil.finance.client.core;
+
+import io.github.jvlealc.hgbrasil.finance.client.model.CurrenciesResponse;
+import io.github.jvlealc.hgbrasil.finance.client.HGBrasilClient;
+import tools.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+/**
+ * Implementa operações para busca do câmbio de moedas e cotação do Bitcoin.
+ *
+ * Esta classe NÃO deve ser instancianda diretamente, utilize a classe {@link HGBrasilClient}
+ * para realizar as operações.
+ * */
+public final class DefaultExchangeOperations extends AbstractHGBrasilOperations implements ExchangeOperations {
+
+    private static final String BASE_URL = "https://api.hgbrasil.com/finance?format=json";
+
+    private final String apiKey;
+
+    public DefaultExchangeOperations(String apiKey, HttpClient httpClient, ObjectMapper objectMapper) {
+        super(httpClient, objectMapper);
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public CurrenciesResponse getCurrencies() {
+        String url = BASE_URL + "&field=currencies&key=" + apiKey;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Accept", "application/json")
+                .build();
+
+        return sendRequest(request, CurrenciesResponse.class);
+    }
+}
