767a243b19dddcf0ab6ca95c7aa5a02a440a7bc5
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-05 15:09:58 -0300

    docs: improve documentation in Ibovespa operations

--- src/main/java/io/github/jvlealc/hgbrasil/finance/client/core/IbovespaOperations.java
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/core/IbovespaOperations.java
@@ -3,7 +3,7 @@ package io.github.jvlealc.hgbrasil.finance.client.core;
 import io.github.jvlealc.hgbrasil.finance.client.model.IbovespaResponse;
 
 /**
- * Interface de contrato que realiza operações para obter histórico OHLC e média da IBOVESPA.
+ * Interface de contrato que realiza operações para obter histórico OHLC e média da Ibovespa.
  *
  * @see <a href="https://hgbrasil.com/docs/finance/stocks">Documentação Oficial da HGBrasil - Bolsa de Valores</a>
  * */
