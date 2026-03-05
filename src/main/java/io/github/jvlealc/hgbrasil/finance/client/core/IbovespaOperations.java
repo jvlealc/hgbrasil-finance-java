897b9493da53d1c78405c51e1b37979880dba6bd
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-05 00:27:44 -0300

    feat: add IbovespaOperations interface

---
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/core/IbovespaOperations.java
@@ -0,0 +1,13 @@
+package io.github.jvlealc.hgbrasil.finance.client.core;
+
+import io.github.jvlealc.hgbrasil.finance.client.model.IbovespaResponse;
+
+/**
+ * Interface de contrato que realiza operações para obter histórico OHLC e média da IBOVESPA.
+ *
+ * @see <a href="https://hgbrasil.com/docs/finance/stocks">Documentação Oficial da HGBrasil - Bolsa de Valores</a>
+ * */
+public interface IbovespaOperations {
+
+    IbovespaResponse getIbovespa();
+}
