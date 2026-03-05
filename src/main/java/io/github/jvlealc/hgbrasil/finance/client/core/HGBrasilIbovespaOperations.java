aa39c9b222720029f487fb05bac6af91226a5558
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-05 00:30:14 -0300

    feat: implement IbovespaOperations

---
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/core/HGBrasilIbovespaOperations.java
@@ -0,0 +1,32 @@
+package io.github.jvlealc.hgbrasil.finance.client.core;
+
+import io.github.jvlealc.hgbrasil.finance.client.model.IbovespaResponse;
+import tools.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class HGBrasilIbovespaOperations extends AbstractHttpExecutor implements IbovespaOperations {
+
+    private static final String BASE_URL = "https://api.hgbrasil.com/finance/ibovespa?format=json";
+
+    private final String apiKey;
+
+    public HGBrasilIbovespaOperations(String apiKey, HttpClient httpClient, ObjectMapper objectMapper) {
+        super(httpClient, objectMapper);
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public IbovespaResponse getIbovespa() {
+        String url = BASE_URL + "&api_key=" + apiKey;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Accept", "application/json")
+                .build();
+
+        return sendRequest(request, IbovespaResponse.class);
+    }
+}
