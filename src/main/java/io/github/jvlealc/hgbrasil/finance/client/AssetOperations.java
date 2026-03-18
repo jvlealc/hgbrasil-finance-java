cae76251e60328e30fc73384593a201a13b20e01
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-18 01:06:42 -0300

    docs: improve Javadoc with exceptions details and return type reference

--- src/main/java/io/github/jvlealc/hgbrasil/finance/client/AssetOperations.java
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/AssetOperations.java
@@ -16,24 +16,33 @@ public interface AssetOperations {
     /**
      * Busca dados de um único ativo.
      *
-     * @param symbol símbolo/ticker do ativo
-     * @return representação de resposta do ativo
+     * @param symbol símbolo do ativo
+     * @return {@link AssetResponse} - modelo de resposta do ativo
+     * @throws NullPointerException se {@code symbol} for nulo
+     * @throws IllegalArgumentException se {@code symbol} for uma string vazia
+     * @throws HGBrasilApiException caso ocorra um erro de rede, falha de autenticação ou erro reportado pela API
      * */
     AssetResponse getBySymbol(String symbol);
 
     /**
-     * Busca dados de de multiplos ativos.
+     * Busca dados de múltiplos ativos.
      *
-     * @param symbols varargs de símbolos/tickers dos ativos
-     * @return representação de resposta dos ativos
+     * @param symbols array de símbolos dos ativos
+     * @return {@link AssetResponse} - modelo de resposta dos ativos
+     * @throws NullPointerException se o array {@code symbols} for nulo
+     * @throws IllegalArgumentException se o array {@code symbols} estiver vazio
+     * @throws HGBrasilApiException caso ocorra um erro de rede, falha de autenticação ou erro reportado pela API
      * */
     AssetResponse getBySymbols(String... symbols);
 
     /**
-     * Busca dados de de multiplos ativos.
+     * Busca dados de múltiplos ativos.
      *
-     * @param symbols lista de símbolos/tickers dos ativos
-     * @return representação de resposta dos ativos
+     * @param symbols lista de símbolos dos ativos
+     * @return {@link AssetResponse} - modelo de resposta dos ativos
+     * @throws NullPointerException se a lista {@code symbols} for nula
+     * @throws IllegalArgumentException se a lista {@code symbols} estiver vazia
+     * @throws HGBrasilApiException caso ocorra um erro de rede, falha de autenticação ou erro reportado pela API
      * */
     AssetResponse getBySymbols(List<String> symbols);
 }
