3a7ac8914d6e00ca6eed6027eacb8cd28f9ffdc7
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-19 01:45:02 -0300

    test: migrate dividend validation tests to AbstractTickerOperationsTest

--- src/test/java/io/github/jvlealc/hgbrasil/finance/client/HGBrasilDividendOperationsTest.java
+++ src/test/java/io/github/jvlealc/hgbrasil/finance/client/HGBrasilDividendOperationsTest.java
@@ -1,24 +1,20 @@
 package io.github.jvlealc.hgbrasil.finance.client;
 
-import io.github.jvlealc.hgbrasil.finance.client.model.DividendResponse;
-import io.github.jvlealc.hgbrasil.finance.client.model.DividendResult;
-import io.github.jvlealc.hgbrasil.finance.client.model.DividendSeries;
+import io.github.jvlealc.hgbrasil.finance.client.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -27,20 +23,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HGBrasilDividendOperationsTest {
 
-    private static final String MOCK_API_KEY = "fakeKey";
+    private static final String FAKE_API_KEY = "fakeKey";
     private static final ObjectMapper OBJECT_MAPPER = JsonMapper.builder()
             .addModule(new JavaTimeModule())
             .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
             .build();
-
-    @Captor
-    ArgumentCaptor<HttpRequest> requestCaptor;
 
     @Mock
     private HttpClient httpClientMock;
@@ -52,73 +44,7 @@ class HGBrasilDividendOperationsTest {
 
     @BeforeEach
     void setup() {
-        dividendOperations = new HGBrasilDividendOperations(MOCK_API_KEY, httpClientMock, OBJECT_MAPPER);
-    }
-
-    @Test
-    @DisplayName("Should throw NullPointerException when ticker is null")
-    void shouldThrowException_whenTickerIsNull () {
-        assertThrows(NullPointerException.class, () ->
-                dividendOperations.getByTicker(null),
-                "Must have thrown NullPointerException"
-        );
-    }
-
-    @Test
-    @DisplayName("Should throw IllegalArgumentException when ticker is blank")
-    void shouldThrowException_whenTickerIsBlank () {
-        assertThrows(IllegalArgumentException.class, () ->
-                dividendOperations.getByTicker("  "),
-                "Must have thrown IllegalArgumentException"
-        );
-    }
-
-    @Test
-    @DisplayName("Should throw IllegalArgumentException when tickers list is empty")
-    void shouldThrowException_whenTickersIsEmpty() {
-        assertThrows(IllegalArgumentException.class, () ->
-                dividendOperations.getByTickers(List.of()),
-                "Must have thrown IllegalArgumentException"
-        );
-    }
-
-    @Test
-    @DisplayName("Should throw IllegalArgumentException when tickers array is equal or less than 0")
-    void shouldThrowException_whenTickersEqualOrLessThanZero() {
-        String[] tickers = {};
-
-        assertThrows(IllegalArgumentException.class, () ->
-                dividendOperations.getByTickers(tickers),
-                "Must have thrown IllegalArgumentException"
-        );
-    }
-
-    @Test
-    @DisplayName("Should throw NullPointerException when historical query params (date, startDate or endDate) is null")
-    void shouldThrowException_whenHistoricalQueryParamIsNull() {
-        assertThrows(NullPointerException.class, () ->
-                dividendOperations.getHistorical("B3:PETR4", LocalDate.now(), null),
-                "Must have thrown NullPointerException"
-        );
-
-        assertThrows(NullPointerException.class, () ->
-                dividendOperations.getHistorical("B3:PETR4", null, LocalDate.now()),
-                "Must have thrown NullPointerException"
-        );
-
-        assertThrows(NullPointerException.class, () ->
-                dividendOperations.getHistorical("B3:PETR4", null),
-                "Must have thrown NullPointerException"
-        );
-    }
-
-    @Test
-    @DisplayName("Should throw IllegalArgumentException when historical query params daysAgo is invalid")
-    void shouldThrowException_whenHistoricalDaysAgoIsInvalid() {
-        assertThrows(IllegalArgumentException.class, () ->
-                dividendOperations.getHistorical("B3:PETR4",-1),
-                "Must have thrown IllegalArgumentException"
-        );
+        dividendOperations = new HGBrasilDividendOperations(FAKE_API_KEY, httpClientMock, OBJECT_MAPPER);
     }
 
     @Test
@@ -264,8 +190,8 @@ class HGBrasilDividendOperationsTest {
         List<DividendSeries> safeSeries = validResult.getSafeSeries();
         assertNotNull(safeSeries, "The safe series must not be null");
         assertEquals(2, safeSeries.size(), "The MGLU3 must have 2 series events");
-        assertEquals("bonus_issue",  safeSeries.getFirst().type(), "First event must be bonus issue");
-        assertEquals("dividend",  safeSeries.get(1).type(), "Second event must be dividend");
+        assertEquals(DividendType.BONUS_ISSUE,  safeSeries.getFirst().type(), "First event must be bonus issue");
+        assertEquals(DividendType.DIVIDEND,  safeSeries.get(1).type(), "Second event must be dividend");
     }
 
     @Test
@@ -338,63 +264,10 @@ class HGBrasilDividendOperationsTest {
                         "com_date must be equal to '2025-12-22'"
                 ),
                 () -> assertEquals(
-                        "approved",
+                        DividendStatus.APPROVED,
                         actualResponse.results().getFirst().series().getFirst().status(),
                         "status must be equal to 'approved'"
                 )
-        );
-    }
-
-    @Test
-    @DisplayName("Should build correct URI query parameters when requesting historical by date range")
-    void shouldBuildCorrectUri_whenGetHistoricalByDateRange() throws IOException, InterruptedException {
-        mockHttpResponse("{}");
-
-        LocalDate startDate = LocalDate.of(2025, 1, 1);
-        LocalDate endDate = LocalDate.of(2025, 12, 31);
-
-        dividendOperations.getHistorical("B3:PETR4", startDate, endDate);
-
-        verify(httpClientMock).send(requestCaptor.capture(), any());
-        URI generatedUri = requestCaptor.getValue().uri();
-
-        assertAll("Verify URI parameters for date range",
-                () -> assertTrue(generatedUri.toString().contains("&tickers=B3:PETR4")),
-                () -> assertTrue(generatedUri.toString().contains("&start_date=2025-01-01")),
-                () -> assertTrue(generatedUri.toString().contains("&end_date=2025-12-31"))
-        );
-    }
-
-    @Test
-    @DisplayName("Should build correct URI query parameters when requesting historical by days ago")
-    void shouldBuildCorrectUri_whenGetHistoricalByDaysAgo() throws IOException, InterruptedException {
-        mockHttpResponse("{}");
-
-        dividendOperations.getHistorical("B3:PETR4", 90);
-
-        verify(httpClientMock).send(requestCaptor.capture(), any());
-        URI generatedUri = requestCaptor.getValue().uri();
-
-        assertAll("Verify URI parameters for days_ago",
-                () -> assertTrue(generatedUri.toString().contains("&tickers=B3:PETR4")),
-                () -> assertTrue(generatedUri.toString().contains("&days_ago=90"))
-        );
-    }
-
-    @Test
-    @DisplayName("Should build correct URI query parameters when requesting historical by single date")
-    void shouldBuildCorrectUri_whenGetHistoricalByDate() throws IOException, InterruptedException {
-        mockHttpResponse("{}");
-        LocalDate date = LocalDate.of(2025, 6, 9);
-
-        dividendOperations.getHistorical("B3:PETR4", date);
-
-        verify(httpClientMock).send(requestCaptor.capture(), any());
-        URI generatedUri = requestCaptor.getValue().uri();
-
-        assertAll("Verify URI parameters for date",
-                () -> assertTrue(generatedUri.toString().contains("&tickers=B3:PETR4")),
-                () -> assertTrue(generatedUri.toString().contains("&date=2025-06-09"))
         );
     }
 
