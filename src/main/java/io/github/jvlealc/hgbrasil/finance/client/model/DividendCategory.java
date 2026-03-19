4f9d1aed59624057d3606c0f71cf48eef794a7f4
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-19 01:30:49 -0300

    docs: add javadoc for DividendCategory

--- src/main/java/io/github/jvlealc/hgbrasil/finance/client/model/DividendCategory.java
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/model/DividendCategory.java
@@ -3,6 +3,11 @@ package io.github.jvlealc.hgbrasil.finance.client.model;
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Representa a categoria do provento distribuído.
+ * Indica se o acionista receberá o benefício em formato de dinheiro
+ * ou em formato de novas ações/ativos.
+ */
 public enum DividendCategory {
     @JsonProperty("cash")
     CASH,
