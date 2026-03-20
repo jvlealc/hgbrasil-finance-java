aa7b1ab2e59caf09040439d318313ad9cdf4b5d5
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-19 23:32:33 -0300

    refactor: convert ExchangeRates to record and add getSafeRates()

--- src/main/java/io/github/jvlealc/hgbrasil/finance/client/model/ExchangeRates.java
+++ src/main/java/io/github/jvlealc/hgbrasil/finance/client/model/ExchangeRates.java
@@ -2,45 +2,24 @@ package io.github.jvlealc.hgbrasil.finance.client.model;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Modelo que encapsula múltiplas taxas de câmbio
  * */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ExchangeRates {
+public record ExchangeRates(
+        String source,
 
-    private String source;
-
-    private final Map<String, Currency> rates = new HashMap<>();
-
-    @JsonProperty("source")
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    @JsonAnySetter
-    public void addRate(String key, Currency currency) {
-        if ("source".equalsIgnoreCase(key)) return;
-        rates.put(key, currency);
-    }
-
-    public Map<String, Currency> getRates() {
-        return this.rates;
-    }
-
-    @Override
-    public String toString() {
-        return "CurrencyData{" +
-                "source='" + source + '\'' +
-                ", rates=" + rates +
-                '}';
+        @JsonAnySetter
+        Map<String, Currency> rates
+){
+    /**
+     * Retorna o Map 'rates', garantindo que nunca seja nulo para evitar {@link NullPointerException}.
+     * @return Map com os resultados do câmbio ou um Map vazio se não houver dados.
+     */
+    public Map<String, Currency> getSafeRates() {
+        return rates != null ? rates : Map.of();
     }
 }
