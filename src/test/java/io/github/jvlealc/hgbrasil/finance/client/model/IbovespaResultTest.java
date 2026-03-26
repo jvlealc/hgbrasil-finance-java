bf787f6d194aa6bd5c33fe1848e8f0d28bcb4b0a
João Vitor Leal de Castro <jv.leal.dev@gmail.com>
2026-03-26 15:40:27 -0300

    test: add unit tests for IbovespaResult

---
+++ src/test/java/io/github/jvlealc/hgbrasil/finance/client/model/IbovespaResultTest.java
@@ -0,0 +1,49 @@
+package io.github.jvlealc.hgbrasil.finance.client.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IbovespaResultTest {
+
+    @Test
+    @DisplayName("Should return empty list when data is null")
+    void shouldReturnEmpty_whenDataIsNull() {
+        IbovespaResult result = createIbovespaResult(null);
+        List<IbovespaIntradayPoint> safeDatas = result.getSafeData();
+        
+        assertNotNull(safeDatas);
+        assertTrue(safeDatas.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return empty list when data is empty")
+    void shouldReturnEmpty_whenDataIsEmpty() {
+        IbovespaResult result = createIbovespaResult(List.of());
+
+        assertTrue(result.getSafeData().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should return mapped data list when API responds success")
+    void shouldReturnData_whenApiRespondsSuccessfully() {
+        IbovespaIntradayPoint firstPoint = new IbovespaIntradayPoint(null, null, null);
+        IbovespaIntradayPoint secondPoint = new IbovespaIntradayPoint(null, null, null);
+        IbovespaResult result = createIbovespaResult(List.of(firstPoint, secondPoint));
+
+        List<IbovespaIntradayPoint> dataPoints = result.getSafeData();
+
+        assertEquals(2, dataPoints.size());
+        assertEquals(firstPoint, dataPoints.getFirst());
+    }
+
+    private static IbovespaResult createIbovespaResult(List<IbovespaIntradayPoint> data) {
+        return new IbovespaResult(
+                null, null, null, null, null, null, null,
+                null, null, data
+        );
+    }
+}
